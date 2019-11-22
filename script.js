91288ec1b613c98ad836cb7502d481698462af0c
Max McCord <maxdmccord@gmail.com>
2019-11-21 23:54:37 -0600

    must turn in - storage does not work and colors do not show

--- script.js
+++ script.js
@@ -23,70 +23,140 @@
 const currentDayEl = document.getElementById("currentDay");
 let time
 
-let hours = [{
+
+
+const hours = [{
     hour: "9a",
-    value: 9
+    value: 9,
+    text: ""
+
 },
 {
     hour: "10a",
-    value: 10
+    value: 10,
+    text: ""
 },
 {
     hour: "11a",
-    value: 11
+    value: 11,
+    text: ""
 },
 {
     hour: "12n",
-    value: 12
+    value: 12,
+    text: ""
 },
 {
     hour: "1p",
-    value: 13
+    value: 13,
+    text: ""
 },
 {
     hour: "2p",
-    value: 14
+    value: 14,
+    text: ""
 },
 {
     hour: "3p",
-    value: 15
+    value: 15,
+    text: ""
 },
 {
     hour: "4p",
-    value: 16
+    value: 16,
+    text: ""
 }]
 
-function clock() {
 
-    interval = setInterval(function () {
+function initCalendar(){
 
-        time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    let dayHour = moment().format('H');
+    console.log(dayHour);
 
-        currentDayEl.innerHTML = time
-
-    }, 1000);
-
-}
-clock()
-
-console.log(hours);
-
-
-for (let i =0; i < hours.length; i++){
-    const calendarSpace = document.getElementById("calendar-space");
-    const rowEl = document.createElement("row");
-    rowEl.setAttribute("class", "row");
-    const columnHourEl = document.createElement("textarea");
-    const columnSaveEl = document.createElement("button");
-    columnHourEl.setAttribute("class", "hour col-8 border border-dark");
-    columnHourEl.setAttribute("placeholder", hours[i].hour);
-    // columnHourEl.innerText = hours[i].hour.value;
-    columnSaveEl.setAttribute("class", "saveBtn col-2");
-    columnSaveEl.innerText = "Save";
+    function clock() {
+        
+        interval = setInterval(function () {
+            
+            time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+            
+            currentDayEl.innerHTML = time
+            
+        }, 1000);
+        
+    }
+    clock()
     
-    console.log(hours[i].hour);
-    calendarSpace.append(rowEl);
-    rowEl.append(columnHourEl);
-    rowEl.append(columnSaveEl);
-}
+    console.log(hours);
+    
+    
+    for (let i = 0; i < hours.length; i++) {
+        const calendarSpace = document.getElementById("calendar-space");
+        const rowEl = document.createElement("row");
+        rowEl.setAttribute("class", "row");
+        const columnHourEl = document.createElement("textarea");
+        const columnSaveEl = document.createElement("button");
+        columnHourEl.setAttribute("class", "hour col-8 border border-dark");
+        columnHourEl.setAttribute("placeholder", hours[i].hour);
+        columnHourEl.setAttribute("id", hours[i].value);
+        // columnHourEl.innerText = hours[i].hour.value;
+        columnSaveEl.setAttribute("class", "saveBtn col-2");
+        columnSaveEl.innerText = "Save";
+        
+        // console.log(hours[i].value);
+        calendarSpace.append(rowEl);
+        rowEl.append(columnHourEl);
+        rowEl.append(columnSaveEl);
+        
+        console.log(columnHourEl);
+        
+        const saveButtons = document.querySelectorAll(".saveBtn");
+        // console.log(saveButtons);
+        let input = document.getElementById(hours[i].value);
 
+
+        //local storage piece is not functioning - I will need major assistance with this portion
+    
+        // if (localStorage){
+
+        //     columnHourEl.innerText = localStorage.getItem("input", hours[i].text);
+        // }
+
+        // console.log(input);
+        // console.log(hours);
+        
+        saveButtons[i].addEventListener("click", function () {
+            hours[i].text = input.value;
+            console.log(hours);       
+            localStorage.setItem("input", hours[i].text)      
+        })
+        const userInputStr = localStorage.getItem(hours[i].text);
+        const userInput = JSON.parse(userInputStr)
+        console.log(userInput);
+        localStorage.setItem("input", JSON.stringify(userInput));
+
+//Ran out of time for the colorization as well
+//         if (hours[i].value < dayHour){
+//             columnHourEl.setAttribute("class", "hour col-8 border border-dark past");
+//         }else if( columnHourEl[i].value === dayHour){
+//             columnHourEl.setAttribute("class", "hour col-8 border border-dark present");
+//         }else if( columnHourEl[i].value > dayHour){
+//             columnHourEl.setAttribute("class", "hour col-8 border border-dark future");
+//         }
+
+// console.log(columnHourEl[i].value)
+    }
+   
+
+        
+        
+    
+}
+initCalendar()
+    
+    
+    
+    
+    
+    
+    
+    
